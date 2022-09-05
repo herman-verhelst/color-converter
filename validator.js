8a41437dd0f16e8d752d3fde1223d660db762128
herman-verhelst <herman.verhelst01@gmail.com>
2022-09-05 11:24:22 +0200

    feat: validate hex input

--- validator.js
+++ validator.js
@@ -2,12 +2,12 @@ const RGB_MAX_VALUE = 255;
 const RGB_MIN_VALUE = 0;
 
 /**
- * Checks if provided input is correct
+ * Checks if provided RGB input is correct
  * @param {string} input - User input
  * @returns {string | boolean} Error message if invalid user input | True if valid user input
- * */
+ */
 export function validateRgbInput(input) {
-    const message = '😞 You should provide three numbers between 0 and 255 seperated by a space...';
+    const message = 'You should provide three numbers between 0 and 255 seperated by a space...';
 
     // Check if only numbers are provided
     if (!/^\d+$/.test(input.replace(/\s/g, ''))) return message;
@@ -24,6 +24,23 @@ export function validateRgbInput(input) {
     return true;
 }
 
+/**
+ * Checks if provided hex input is correct
+ * @param {string} input - User input
+ * @returns {string | boolean} Error message if invalid user input | True if valid user input
+ */
 export function validateHexInput(input) {
+    const message = 'You should provide a valid hex color value...';
+
+    // Check if input is of desired length
+    if (input.length !== 7) return message;
+
+    // Check if first character is a #
+    if (input[0] !== '#') return message;
+
+    // Check if last characters are valid hex values
+    const regex = /[0-9A-Fa-f]{6}/g;
+    if (!regex.test(input.substring(1))) return message;
+
     return true;
 }
